6b6f2c0e52389fab49bf0906ce701bffbc1a4117
DefinitelynotZombie <davidmuoegbunam@gmail.com>
2024-08-19 20:28:26 +0100

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import HomePage from "./pages/HomePage";
-import BlogPost from "./pages/Blogpost";
+import BlogPost from "./pages/BlogPost";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 function App() {
